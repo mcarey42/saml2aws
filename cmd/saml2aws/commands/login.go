43309c7c0c07f1a7b9622b327ac2e95ac1543908
Mark Carey <mcarey@solcore.net>
2026-03-10 09:18:52 -0400

    SDK porting issue resolved around eager load of profile files.  Any errors will cause a failuter.  Since we are already authenticated, we do not need to consider the profile files during configuration load.

--- cmd/saml2aws/commands/login.go
+++ cmd/saml2aws/commands/login.go
@@ -361,8 +361,15 @@ func resolveRole(awsRoles []*saml2aws.AWSRole, samlAssertion string, account *cf
 func loginToStsUsingRole(account *cfg.IDPAccount, role *saml2aws.AWSRole, samlAssertion string) (*awsconfig.AWSCredentials, error) {
 	ctx := context.Background()
 
+	// SDK v1 -> v2 porting note: Unlike SDK v1's session.NewSession, the v2 SDK's
+	// LoadDefaultConfig eager loads and validates *all* profiles.  Any profile errors
+	// become fatal.  Since we're already authenticated, we don't actually need any aditional 
+	// information from the Config/Cred files, so we can just pass empty arrays and resolve
+	// the problem.
 	awsCfg, err := config.LoadDefaultConfig(ctx,
 		config.WithRegion(account.Region),
+		config.WithSharedConfigFiles([]string{}),
+		config.WithSharedCredentialFiles([]string{}),
 	)
 	if err != nil {
 		return nil, errors.Wrap(err, "Failed to create session.")
